f29aa7b7794e522496015b1b789aa1c5dc5d62cf
anonymze <anodevfr@gmail.com>
2024-09-25 15:54:30 +0200

    feat: theme app

--- src/layouts/Header/_components/Accessories.tsx
+++ src/layouts/Header/_components/Accessories.tsx
@@ -2,17 +2,17 @@ import { accessories } from "../_navbar";
 import "./Accessories.css";
 
 const Accessories = () => {
-	return (
-		<>
-			{accessories.map((accessory) => {
-				return (
-					<li key={accessory.id}>
-						<button onClick={accessory.on_click}>{accessory.label}</button>
-					</li>
-				);
-			})}
-		</>
-	);
+  return (
+    <>
+      {accessories.map((accessory) => {
+        return (
+          <li key={accessory.id}>
+            <button onClick={accessory.on_click}>{accessory.label}</button>
+          </li>
+        );
+      })}
+    </>
+  );
 };
 
 export default Accessories;
--- src/layouts/Header/_navbar.ts
+++ src/layouts/Header/_navbar.ts
@@ -1,45 +1,61 @@
 interface Nav {
-	id: number,
-	url: string;
-	label: string;
-	icon: string | null;
+  id: number;
+  url: string;
+  label: string;
+  icon: string | null;
 }
 
 interface Accessory {
-	id: number,
-	icon: string;
-	label: string;
-	on_click: () => void;
+  id: number;
+  icon: string;
+  label: string;
+  on_click: () => void;
 }
 
 //
 export const navs: Array<Nav> = [
-	{
-		id: 1,
-		url: "/projects",
-		label: "Projects",
-		icon: null,
-	},
-	{
-		id: 2,
-		url: "/extra",
-		label: "Extra",
-		icon: null,
-	},
+  {
+    id: 1,
+    url: "/projects",
+    label: "Projects",
+    icon: null,
+  },
+  {
+    id: 2,
+    url: "/extra",
+    label: "Extra",
+    icon: null,
+  },
 ];
 
 //
 export const accessories: Array<Accessory> = [
-	{
-		id: 1,
-		label: "switch_light",
-		icon: "",
-		on_click: () => {console.log(window);},
-	},
-	{
-		id: 2,
-		label: "mail",
-		icon: "",
-		on_click: () => {},
-	},
+  {
+    id: 1,
+    label: "switch_light",
+    icon: "",
+    on_click: () => {
+      const fixed_theme = document.body.getAttribute("data-theme");
+
+      if (!fixed_theme) {
+        const natural_theme = window.matchMedia("(prefers-color-scheme: dark)");
+
+        natural_theme.matches
+          ? document.body.setAttribute("data-theme", "light")
+          : document.body.setAttribute("data-theme", "dark");
+
+        return;
+      }
+
+      fixed_theme === "dark"
+        ? document.body.setAttribute("data-theme", "light")
+        : document.body.setAttribute("data-theme", "dark");
+    },
+  },
+  {
+    id: 2,
+    label: "mail",
+    icon: "",
+    on_click: () => {},
+  },
 ];
