9f2786b36c526835c77edae4a42fb42b54506d92
anonymze <anodevfr@gmail.com>
2024-09-27 11:27:08 +0200

    refacto: switch theme

--- src/utils/theme_browser.ts
+++ src/utils/theme_browser.ts
@@ -2,26 +2,51 @@ import type { Theme } from "@/types/theme";
 
 type AttributeName = `data-${string}`;
 
-export const setTheme = (attribute_theme: AttributeName) => {
-	const fixed_theme = document.body.getAttribute(
-		attribute_theme,
-	) as Theme | null;
+const storage_name_fix = "theme";
 
-	if (!fixed_theme) {
-		const natural_theme = window.matchMedia("(prefers-color-scheme: dark)");
+export const setTheme = (
+  attribute_theme: AttributeName,
+  storage_name?: string,
+) => {
+  const fixed_theme = document.body.getAttribute(
+    attribute_theme,
+  ) as Theme | null;
 
-		natural_theme.matches
-			? document.body.setAttribute(attribute_theme, "light")
-			: document.body.setAttribute(attribute_theme, "dark");
-	} else {
-		fixed_theme === "dark"
-			? document.body.setAttribute(attribute_theme, "light")
-			: document.body.setAttribute(attribute_theme, "dark");
-	}
+  if (!fixed_theme) {
+    const natural_theme = window.matchMedia("(prefers-color-scheme: dark)");
 
-	window.sessionStorage.setItem("theme", document.body.getAttribute(attribute_theme)!);
+    natural_theme.matches
+      ? document.body.setAttribute(attribute_theme, "light")
+      : document.body.setAttribute(attribute_theme, "dark");
+  } else {
+    fixed_theme === "dark"
+      ? document.body.setAttribute(attribute_theme, "light")
+      : document.body.setAttribute(attribute_theme, "dark");
+  }
+
+  window.sessionStorage.setItem(
+    storage_name ?? storage_name_fix,
+    document.body.getAttribute(attribute_theme)!,
+  );
 };
 
-export const getTheme = (attribute_theme: AttributeName): Theme => {
-  return "light";
+export const getTheme = (
+  attribute_theme: AttributeName,
+  storage_name?: string,
+): Theme => {
+  const fixed_theme = document.body.getAttribute(
+    attribute_theme,
+  ) as Theme | null;
+
+  if (fixed_theme) return fixed_theme;
+
+  const storage_theme = window.sessionStorage.getItem(
+    storage_name ?? storage_name_fix,
+  ) as Theme | null;
+
+  if (storage_theme) return storage_theme;
+
+  return window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
 };
