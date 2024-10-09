d32bb78f6120be47c5117b5ba9aa14635f338280
anonymze <anodevfr@gmail.com>
2024-10-09 12:57:55 +0200

    fix: replace bun by npm

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,16 +1,16 @@
 // we can streat svg like Components in React with the help of the plugin svgr in the vite config
-// declare module '*.svg?react' {
-// 	import React from 'react';
+declare module '*.svg?react' {
+	import React from 'react';
 
-// 	interface SvgProps extends React.SVGProps<SVGElement> {
-// 			// Add any custom props you want here
-// 	}
+	interface SvgProps extends React.SVGProps<SVGElement> {
+			// Add any custom props you want here
+	}
 
-// 	const content: React.FC<SvgProps>;
-// 	export default content;
-// }
+	const content: React.FC<SvgProps>;
+	export default content;
+}
 
-// type SVGIcons = "dark" | "light" | "mail" | "linkedin" | "location" | "flag-en" | "flag-fr" | "magic" | "hat-popover" | "expo" | "react" | "typescript" | "tailwind";
+type SVGIcons = "dark" | "light" | "mail" | "linkedin" | "location" | "flag-en" | "flag-fr" | "magic" | "hat-popover" | "expo" | "react" | "typescript" | "tailwind";
 
 
 
